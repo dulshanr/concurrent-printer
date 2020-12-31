b556fb425390d703cd32d05e1789d65162571811
unknown <dulshanr12@gmail.com>
2020-12-31 05:55:23 +0530

    removed unnecessary files

--- src/normal/Tester.java
+++
@@ -1,9 +0,0 @@
-package normal;
-
-public class Tester {
-    public static void main(String[] args) {
-        ParentClass p1 = new ParentClass();
-        p1.callMe();
-        ParentClass p2 = new ParentClass();
-    }
-}
